2233d4b6142166234a20f96fd639110b0f6de3ab
Ryan <ryan.piinheiro@hotmail.com>
2024-10-29 19:29:20 -0300

    Componente Resumo

---
+++ src/components/Resumo/resumo.tsx
@@ -0,0 +1,32 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { ResumoContainer, ResumoCard } from "./styles";
+
+export function Resumo() {
+    return(
+        <ResumoContainer>
+            <ResumoCard>
+                <header>
+                    <span>Entradas</span>
+                    <ArrowCircleUp  size={32} color="#00b37e" />
+                </header>
+                <strong>R$ 1.000,00</strong>
+            </ResumoCard>
+
+            <ResumoCard>
+                <header>
+                    <span>Saidas</span>
+                    <ArrowCircleDown  size={32} color="#f75a68" />
+                </header>
+                <strong>R$ 500,00</strong>
+            </ResumoCard>
+
+            <ResumoCard variant="green">
+                <header>
+                    <span>Total</span>
+                    <CurrencyDollar  size={32} color="#fff" />
+                </header>
+                <strong>R$ 500,00</strong>
+            </ResumoCard>
+        </ResumoContainer>
+    )
+}
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/Header/Header";
+import { Resumo } from "../../components/Resumo/resumo";
 
 export function Transactions() {
     return (
         <div>
             <Header />
+            <Resumo />
         </div>
     )
 }
