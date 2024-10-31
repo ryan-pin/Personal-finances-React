d64f67f777431bdfd9feec5d3c400e8431c4fbd0
Ryan <ryan.piinheiro@hotmail.com>
2024-10-30 21:33:04 -0300

    Radix-ui MODAL

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog';
 
 export function Header() {
     return (
@@ -6,7 +7,22 @@ export function Header() {
             <HeaderContent>
                 <h1>Personal Finances</h1>
 
-                <NewTransactionButton>Nova transação</NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                       <NewTransactionButton>Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <Dialog.Portal>
+                        <Dialog.Overlay />
+                        <Dialog.Content>
+                            <Dialog.Title>Nova transação</Dialog.Title>
+
+                            <Dialog.Close />
+                        </Dialog.Content>
+                    </Dialog.Portal>
+
+                </Dialog.Root>
+                
             </HeaderContent>
         </HeaderContainer>
     )
