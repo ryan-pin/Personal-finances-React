59a4480312e7758f3d7bc6722a8c7f46aa28d835
Ryan <ryan.piinheiro@hotmail.com>
2024-11-18 19:42:48 -0300

    final

--- src/contexts/TransacoesContext.tsx
+++ src/contexts/TransacoesContext.tsx
@@ -64,7 +64,7 @@ export function TransacoesProvider({ children }: TransacoesProviderProps) {
 
   useEffect(() => {
     fetchTransactions();
-  }, []);
+  }, [fetchTransactions]);
 
   return (
     <TransacaoContext.Provider
